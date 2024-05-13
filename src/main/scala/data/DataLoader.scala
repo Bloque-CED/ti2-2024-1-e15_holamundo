0dd6709607e7b13cb3980a076787e11632b7fcdd
John Hortua <johnhortua63@gmail.com>
2024-05-12 21:35:04 -0500

    Added javadoc

--- src/main/scala/data/DataLoader.scala
+++ src/main/scala/data/DataLoader.scala
@@ -2,6 +2,11 @@ package data
 
 class DataLoader {
   
+  /**
+   * Load data from a file
+   * @param path the path to the file
+   * @return the data in the file
+   */
   def loadData(path : String): String = {
     val source = scala.io.Source.fromFile(path)
     val lines = try source.mkString finally source.close()
