4a0a699378cbe7a362f0849e8853f3904aac175a
John Hortua <johnhortua63@gmail.com>
2024-05-12 21:36:08 -0500

    Now testing file creation and accuracy

--- src/test/scala/DataGeneratorTest.scala
+++ src/test/scala/DataGeneratorTest.scala
@@ -3,60 +3,41 @@ import munit.FunSuite
 class DataGeneratorTest extends FunSuite {
   
   // Las entradas están categorizadas de acuerdo a su tamaño así: 
-  //Toy, n<10^2
-  //Small, 10^2<=n<10^4
-  //Medium, 10^4<=n<10^5
-  //Large, n<10^6
+  //Toy, n<10^2  set value = 99
+  //Small, 10^2<=n<10^4 set value = 9999
+  //Medium, 10^4<=n<10^5 set value = 99999
+  //Large, n<10^6 set value = 1000000
   
-  test("DataGenerator should generate a string of length 99") {
-    val dataGenerator = new DataGenerator()
-    val generatedData = dataGenerator.generateData("Toy")
-    assertEquals(generatedData.length, 99)
+  val dataGenerator = new data.DataGenerator()
+  val dataLoader = new data.DataLoader()
+  
+  test("Generate data file with Toy size") {
+    dataGenerator.generateData("ToyData", "Toy")
+    val data = dataLoader.loadData("src/main/resources/data/ToyData.txt")
+    assert(data.length == 99)
   }
   
-  test("DataGenerator should generate a string of length 9999") {
-    val dataGenerator = new DataGenerator()
-    val generatedData = dataGenerator.generateData("Small")
-    assertEquals(generatedData.length, 9999)
+  test("Generate data file with Small size") {
+    dataGenerator.generateData("SmallData", "Small")
+    val data = dataLoader.loadData("src/main/resources/data/SmallData.txt")
+    assert(data.length == 9999)
   }
-  
-  test("DataGenerator should generate a string of length 99999") {
-    val dataGenerator = new DataGenerator()
-    val generatedData = dataGenerator.generateData("Medium")
-    assertEquals(generatedData.length, 99999)
-  }
-  
-  test("DataGenerator should generate a string of length 999999") {
-    val dataGenerator = new DataGenerator()
-    val generatedData = dataGenerator.generateData("Large")
-    assertEquals(generatedData.length, 9999999)
-  }
-  
-  test("DataGenerator should generate an empty string for an invalid category") {
-    val dataGenerator = new DataGenerator()
-    val generatedData = dataGenerator.generateData("Invalid")
-    assertEquals(generatedData.length, 0)
-  }
-  
-  test("DataGenerator should generate an empty string for a negative length") {
-    val dataGenerator = new DataGenerator()
-    val length = -100
-    val generatedData = dataGenerator.generateData(length)
-    assertEquals(generatedData.length, 0)
-  }
-  
-  test("DataGenerator should generate an empty string for a length of 0") {
-    val dataGenerator = new DataGenerator()
-    val length = 0
-    val generatedData = dataGenerator.generateData(length)
-    assertEquals(generatedData.length, 0)
-  }
-  
-  test("DataGenerator should generate a string of arbitrary length") {
-    val dataGenerator = new DataGenerator()
-    val length = 12345
-    val generatedData = dataGenerator.generateData(length)
-    assertEquals(generatedData.length, length)
+ 
+  test("Generate data file with Medium size") {
+    dataGenerator.generateData("MediumData", "Medium")
+    val data = dataLoader.loadData("src/main/resources/data/MediumData.txt")
+    assert(data.length == 99999)
   }
 
+  test("Generate data file with Large size") {
+    dataGenerator.generateData("LargeData", "Large")
+    val data = dataLoader.loadData("src/main/resources/data/LargeData.txt")
+    assert(data.length == 1000000)
+  }
+ 
+  test("Generate data file with specified length") {
+    dataGenerator.generateData("SpecifiedData", 100)
+    val data = dataLoader.loadData("src/main/resources/data/SpecifiedData.txt")
+    assert(data.length == 100)
+  }
 }
