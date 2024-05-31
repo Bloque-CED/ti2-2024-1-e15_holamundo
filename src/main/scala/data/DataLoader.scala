5f80d4b55ad03a0025b7de0a0dcd47501259dbe5
Damy <damyvillegas7@gmail.com>
2024-05-31 02:04:54 -0500

    Error DataLoader

--- src/main/scala/data/DataLoader.scala
+++ src/main/scala/data/DataLoader.scala
@@ -8,34 +8,35 @@ import data.DataGenerator
 
 class DataLoader {
 
-  def load(filePath: String): Array[Byte] = {
-    Files.readAllBytes(Paths.get(filePath))
-  }
 
-  def readFromMemory(data: Array[Byte]): Array[Byte] = {
-    data.clone()
-  }
+  //def load(filePath: String): Array[Byte] = {
+    //Files.readAllBytes(Paths.get(filePath))
+  //}
 
-  def saveData(data: Array[Byte], filePath: String)(implicit spark: SparkSession): Unit = {
-    spark.createDataFrame(Seq(("data", data))).write.format("binaryFile").save(filePath)
-  }
+ // def readFromMemory(data: Array[Byte]): Array[Byte] = {
+   // data.clone()
+ // }
 
-  def loadAndCalculateChecksum(filePath: String, checksumCalculator: CheckSumCalculator): Int = {
-    val data = load(filePath)
-    checksumCalculator.calculateChecksum(new String(data))
-  }
+ // def saveData(data: Array[Byte], filePath: String)(implicit spark: SparkSession): Unit = {
+ //   spark.createDataFrame(Seq(("data", data))).write.format("binaryFile").save(filePath)
+ // }
 
-  def loadAndConvertData(filePath: String, converter: Conver): (String, Array[Byte], String) = {
-    val data = load(filePath)
-    val binaryData = converter.stringToBinary(new String(data))
-    val asciiData = converter.stringToAscii(new String(data))
-    (binaryData, data, asciiData)
-  }
+ // def loadAndCalculateChecksum(filePath: String, checksumCalculator: CheckSumCalculator): Int = {
+ //   val data = load(filePath)
+  //  checksumCalculator.calculateChecksum(new String(data))
+  //}
 
-  def generateAndSaveData(category: String, dataGenerator: DataGenerator, dataLoader: DataLoader, filePath: String): Unit = {
-    val data = dataGenerator.generateStringData(category)
-    val dataString = new String(data)
-    dataLoader.saveData(data, filePath)
-    println(s"Data saved to $filePath: $dataString")
-  }
+ // def loadAndConvertData(filePath: String, converter: Conver): (String, Array[Byte], String) = {
+ //   val data = load(filePath)
+  //  val binaryData = converter.stringToBinary(new String(data))
+   // val asciiData = converter.stringToAscii(new String(data))
+  //  (binaryData, data, asciiData)
+  //}
+
+//  def generateAndSaveData(category: String, dataGenerator: DataGenerator, dataLoader: DataLoader, filePath: String): Unit = {
+//    val data = dataGenerator.generateStringData(category)
+  //  val dataString = new String(data)
+  //  dataLoader.saveData(data, filePath)
+  //  println(s"Data saved to $filePath: $dataString")
+  //}
 }
