bf58fdef10347408f0f8e685e43c6bca7256ef1a
Damy <damyvillegas7@gmail.com>
2024-05-31 15:03:25 -0500

    CheckSum

--- src/main/scala/algorithms/CheckSumCalculator.scala
+++ src/main/scala/algorithms/CheckSumCalculator.scala
@@ -2,12 +2,29 @@ package algorithms
 
 class CheckSumCalculator {
 
+  // Función para calcular el checksum de una secuencia de bytes
   def calculate(data: Array[Byte]): Int = {
     data.foldLeft(0)((checksum, byte) => checksum + (byte & 0xFF)) & 0xFF
   }
 
+  // Función para calcular el checksum de una cadena
   def calculateChecksum(data: String): Int = {
     calculate(data.getBytes)
   }
+
+  // Función para verificar la integridad de los datos usando el checksum
+  def verifyChecksumCalculate(data: Array[Byte], checksum: Int): Boolean = {
+    calculate(data) == checksum
+  }
+
+  // Función para verificar la integridad de los datos en cadena usando el checksum
+  def verifyChecksumCalculateChecksum(data: String, checksum: Int): Boolean = {
+    calculateChecksum(data) == checksum
+  }
+
 }
+
+
+
+
 
