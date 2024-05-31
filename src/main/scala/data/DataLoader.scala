f331a5ff63dcb3240743c9a177aa63c9fbe9bc85
Damy <damyvillegas7@gmail.com>
2024-05-31 01:58:53 -0500

    DataLoader

--- src/main/scala/data/DataLoader.scala
+++ src/main/scala/data/DataLoader.scala
@@ -1,6 +1,10 @@
 package data
 
 import java.nio.file.{Paths, Files}
+import algorithms.CheckSumCalculator
+import data.Conver
+import data.DataGenerator
+
 
 class DataLoader {
 
@@ -10,5 +14,28 @@ class DataLoader {
 
   def readFromMemory(data: Array[Byte]): Array[Byte] = {
     data.clone()
+  }
+
+  def saveData(data: Array[Byte], filePath: String)(implicit spark: SparkSession): Unit = {
+    spark.createDataFrame(Seq(("data", data))).write.format("binaryFile").save(filePath)
+  }
+
+  def loadAndCalculateChecksum(filePath: String, checksumCalculator: CheckSumCalculator): Int = {
+    val data = load(filePath)
+    checksumCalculator.calculateChecksum(new String(data))
+  }
+
+  def loadAndConvertData(filePath: String, converter: Conver): (String, Array[Byte], String) = {
+    val data = load(filePath)
+    val binaryData = converter.stringToBinary(new String(data))
+    val asciiData = converter.stringToAscii(new String(data))
+    (binaryData, data, asciiData)
+  }
+
+  def generateAndSaveData(category: String, dataGenerator: DataGenerator, dataLoader: DataLoader, filePath: String): Unit = {
+    val data = dataGenerator.generateStringData(category)
+    val dataString = new String(data)
+    dataLoader.saveData(data, filePath)
+    println(s"Data saved to $filePath: $dataString")
   }
 }
